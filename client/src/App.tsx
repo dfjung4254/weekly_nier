95ab30fa7bc6c4c550ed8bb694a9fb6c6b3fe999
dfjung4254 <rmsghk4254@naver.com>
2020-10-05 02:31:32 +0900

    initial client

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import {Typography} from '@material-ui/core';
+
+function App() {
+  return (
+    <div className="App">
+      <Typography variant="h1">HELLO NIER</Typography>
+      <Typography variant="h3">Weekly Nier's Tech Blog</Typography>
+    </div>
+  );
+}
+
+export default App;
